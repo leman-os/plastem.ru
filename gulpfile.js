816a3db2726a105613c160f4f95afaa0d4ffda94
Sergey Gulin <info@wb-group.ru>
2019-10-18 17:12:43 +0300

    rrr2

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/mask/mask.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
